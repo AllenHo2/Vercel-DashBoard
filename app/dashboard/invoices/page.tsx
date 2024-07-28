2568fe3129b1118a372add974353a5cf74ba93df
AllenHo2 <aho22@dons.usfca.edu>
2024-07-27 21:12:37 -0700

    Added pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
  
 async function Page( 
     {searchParams} 
@@ -18,6 +19,7 @@ async function Page(
 
     const query = searchParams?.query || ""; //search query either has a valye or null
     const currentPage = Number(searchParams?.page) || 1; //current page is either set or default to 1st page
+    const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-c   enter justify-between">
@@ -31,7 +33,7 @@ async function Page(
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
